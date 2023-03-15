f774c69c4e9e4820dcd1e64b3aeed03a256bfc52
RedMoon <SashaM122@mail.ru>
2023-03-15 09:04:21 +0500

    Django project deploy

--- DjangoCrm/DjangoCrm/urls.py
+++ DjangoCrm/DjangoCrm/urls.py
@@ -1,4 +1,4 @@
-"""SimpleCrm URL Configuration
+"""DjangoCrm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- DjangoCrm/manage.py
+++ DjangoCrm/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SimpleCrm.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCrm.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
