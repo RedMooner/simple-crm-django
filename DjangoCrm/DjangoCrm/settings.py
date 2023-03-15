b23c5ac8031d5a2b0e120d09cef2bb93f55233cd
RedMoon <SashaM122@mail.ru>
2023-03-15 10:09:50 +0500

    Add first main app, Create two pages (main,about) Create templates, simple styles Setup routing

--- DjangoCrm/DjangoCrm/settings.py
+++ DjangoCrm/DjangoCrm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
