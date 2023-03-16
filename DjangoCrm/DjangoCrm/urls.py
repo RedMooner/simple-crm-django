bdb6ba5d570d6dc14810a18a40809193e196a94f
RedMoon <SashaM122@mail.ru>
2023-03-16 09:09:18 +0500

    The system of authorization

--- DjangoCrm/DjangoCrm/urls.py
+++ DjangoCrm/DjangoCrm/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path('board', include('Kanban.urls'))
+    path("board", include("Kanban.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),  # Добавили новый марш
+    path("accounts/", include("accounts.urls")),  # Добавили новый марш
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
