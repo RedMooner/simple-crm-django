3f2bf0aa100e31e1acb62bd1b5db9b0470fd0578
RedMoon <SashaM122@mail.ru>
2023-03-16 09:47:07 +0500

    Add todo db model

--- DjangoCrm/Kanban/models.py
+++ DjangoCrm/Kanban/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User # new
+
 
 # Create your models here.
+class TodoChild(models.Model):
+    title = models.CharField(max_length=30, help_text="Enter a todo title")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # newull=True,
+
+    def __unicode__(self):
+        return self.user.id
--- DjangoCrm/accounts/admin.py
+++ DjangoCrm/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from Kanban.models import TodoChild
 
 # Register your models here.
+admin.site.register(TodoChild)
