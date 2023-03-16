157a29c4dcfb658d986d7844e66d4730a6cc0390
RedMoon <SashaM122@mail.ru>
2023-03-16 08:43:19 +0500

    Kanban API and pages deploy

--- DjangoCrm/DjangoCrm/urls.py
+++ DjangoCrm/DjangoCrm/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
+    path('board', include('Kanban.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
