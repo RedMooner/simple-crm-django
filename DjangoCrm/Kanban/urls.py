bde2748c5fd8c2025a6c9d73005c72cc4d8f8488
RedMoon <SashaM122@mail.ru>
2023-03-17 09:14:23 +0500

    Adding tasks to the database

--- DjangoCrm/Kanban/urls.py
+++ DjangoCrm/Kanban/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.index, name="board"),
-]
+urlpatterns = [path("", views.index, name="board"), path("posttodo", views.posttodo)]
