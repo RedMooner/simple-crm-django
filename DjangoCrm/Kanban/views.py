a650161e7a8591c114e3bf2e34f50d248be2f142
RedMoon <SashaM122@mail.ru>
2023-03-17 08:48:50 +0500

    Added user division

--- DjangoCrm/Kanban/views.py
+++ DjangoCrm/Kanban/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import TodoChild
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def index(request):
-    todos = TodoChild.objects.all()
+    todos = TodoChild.objects.filter(user=request.user)
     return render(request, "board.html", {"list": todos})
